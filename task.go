764a837d034a0a977c24879181ee446d696272b9
tahir <tahir@tahirs-MacBook-Pro.local>
2020-11-15 15:24:19 -0500

    Testing

--- task.go
+++ task.go
@@ -1,6 +1,16 @@
 package ci
 
-import "log"
+import (
+	"context"
+	"io"
+	"log"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/stdcopy"
+)
 
 type Step struct {
 	Image string
@@ -9,4 +19,44 @@ type Step struct {
 
 func RunStep(step *Step) {
 	log.Println("Running::", step)
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient() //(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: step.Image,
+		Cmd:   step.Cmd,
+	}, nil, nil, "")
+	if err != nil {
+		panic(err)
+	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	cli.ContainerWait(ctx, resp.ID)
+	//select {
+	//case err := <-errCh:
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//case <-statusCh:
+	//}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
